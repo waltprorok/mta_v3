ce9a0cce146abafd9a97075c64df0eba214bbf22
Walter Prorok <waltprorok.com>
2020-05-04 12:10:37 -0400

    updated composer packages

--- resources/views/webapp/blog/index.blade.php
+++ resources/views/webapp/blog/index.blade.php
@@ -52,8 +52,7 @@
                 <div class="modal-content">
                     <div class="modal-header">
                         <h4 class="modal-title" id="myModalLabel">Delete Blog?</h4>
-                        <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span
-                                    aria-hidden="true">&times;</span></button>
+                        <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
                     </div>
 
                     <div class="modal-body">
