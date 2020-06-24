6d2e546a3a229a13362dd29ec97cca766b355475
Walter Prorok <waltprorok.com>
2020-06-24 11:22:39 -0400

    added captcha to contact form

--- resources/views/marketing/contact.blade.php
+++ resources/views/marketing/contact.blade.php
@@ -50,6 +50,16 @@
                                     </span>
                             @endif
                         </div>
+
+                        <div class="form-group {{ $errors->has('g-recaptcha-response') ? ' has-error' : '' }}">
+                            {!! app('captcha')->display(); !!}
+                            @if ($errors->has('g-recaptcha-response'))
+                                <span class="help-block">
+                                        <strong>{{ $errors->first('g-recaptcha-response') }}</strong>
+                                    </span>
+                            @endif
+                        </div>
+
                         <button class="main-btn" type="submit">Send message</button>
                     </form>
                 </div>
