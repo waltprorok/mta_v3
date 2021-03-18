8ac2b3bd069367586e7f7a89520d82f7173a6782
Walter Prorok <w.prorok@ubreakifix.com>
2021-03-17 22:27:56 -0400

    updated blog controller

--- app/Http/Controllers/BlogController.php
+++ app/Http/Controllers/BlogController.php
@@ -15,7 +15,7 @@ use Illuminate\View\View;
 
 class BlogController extends Controller
 {
-    protected $blogLimit = 6;
+    protected $blogLimit = 3;
 
     /**
      * Display a listing of the resource.
