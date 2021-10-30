ed3a1e01afc641d161fac463d7c2aaa4417eec70
Walter Prorok <w.prorok@ubreakifix.com>
2021-10-30 17:31:42 -0400

    added return types to teacher store settings validator

--- app/Http/Requests/TeacherStoreSettings.php
+++ app/Http/Requests/TeacherStoreSettings.php
@@ -11,7 +11,7 @@ class TeacherStoreSettings extends FormRequest
      *
      * @return bool
      */
-    public function authorize()
+    public function authorize(): bool
     {
         return true;
     }
@@ -21,7 +21,7 @@ class TeacherStoreSettings extends FormRequest
      *
      * @return array
      */
-    public function rules()
+    public function rules(): array
     {
         return [
             'teacher_id' => 'required|integer',
