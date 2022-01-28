4bb6015a4d0ac32cfb5809a7293435ff0701b4e2
Walter Prorok <w.prorok@ubreakifix.com>
2022-01-28 17:54:33 -0500

    MTA-18 added partial message to nav bar

--- resources/views/layouts/webnav.blade.php
+++ resources/views/layouts/webnav.blade.php
@@ -20,18 +20,19 @@
                     </li>
 
                     <li class="nav-item">
+                        <a href="{{ route('webapp.student.lessonsApi') }}"
+                           class="nav-link {{ Route::currentRouteName() == 'webapp.student.lessonsApi' ? 'active' : '' }}">
+                            <i class="fa fa-leaf"></i> Lessons
+                        </a>
+                    </li>
+
+                    <li class="nav-item">
                         <a href="{{ route('webapp.admin.students') }}"
                            class="nav-link {{ Route::currentRouteName() == 'webapp.admin.students' ? 'active' : '' }}">
                             <i class="fa fa-graduation-cap"></i> Students
                         </a>
                     </li>
 
-                    <li class="nav-item">
-                        <a href="{{ route('webapp.student.lessonsApi') }}"
-                           class="nav-link {{ Route::currentRouteName() == 'webapp.student.lessonsApi' ? 'active' : '' }}">
-                            <i class="fa fa-leaf"></i> Lessons
-                        </a>
-                    </li>
 
                     <li class="nav-item">
                         <a href="{{ route('webapp.admin.teachers') }}"
@@ -70,63 +71,12 @@
                         </a>
                     </li>
 
-                    <li class="nav-item nav-dropdown">
-                        <a href="#" class="nav-link nav-dropdown-toggle {{ Route::currentRouteName() == '#' ? 'active' : '' }}">
-                            <i class="fa fa-envelope"></i> Messages <i class="fa fa-caret-left"></i>
-                        </a>
-                        <ul class="nav-dropdown-items">
-                            <li class="nav-item">
-                                <a href="{{ route('message.inbox') }}" class="nav-link">
-                                    <i class="fa fa-inbox"></i> Inbox
-                                </a>
-                            </li>
-                            <li class="nav-item">
-                                <a href="{{ route('message.create') }}" class="nav-link">
-                                    <i class="fa fa-pencil"></i> Create
-                                </a>
-                            </li>
-                            <li class="nav-item">
-                                <a href="{{ route('message.sent') }}" class="nav-link">
-                                    <i class="fa fa-paper-plane"></i> Sent
-                                </a>
-                            </li>
-                            <li class="nav-item">
-                                <a href="{{ route('message.deleted') }}" class="nav-link">
-                                    <i class="fa fa-trash"></i> Deleted
-                                </a>
-                            </li>
-                        </ul>
-                    </li>
+                    @include('partials.messageNavBar')
+
                 @endif
 
                 @if(Auth::user()->student)
-                    <li class="nav-item nav-dropdown">
-                        <a href="#" class="nav-link nav-dropdown-toggle {{ Route::currentRouteName() == '#' ? 'active' : '' }}">
-                            <i class="fa fa-envelope"></i> Messages <i class="fa fa-caret-left"></i>
-                        </a>
-                        <ul class="nav-dropdown-items">
-                            <li class="nav-item">
-                                <a href="{{ route('message.inbox') }}" class="nav-link">
-                                    <i class="fa fa-inbox"></i> Inbox
-                                </a>
-                            </li>
-                            <li class="nav-item">
-                                <a href="{{ route('message.create') }}" class="nav-link">
-                                    <i class="fa fa-pencil"></i> Create
-                                </a>
-                            </li>
-                            <li class="nav-item">
-                                <a href="{{ route('message.sent') }}" class="nav-link">
-                                    <i class="fa fa-paper-plane"></i> Sent
-                                </a>
-                            </li>
-                            <li class="nav-item">
-                                <a href="{{ route('message.deleted') }}" class="nav-link">
-                                    <i class="fa fa-trash"></i> Deleted
-                                </a>
-                            </li>
-                        </ul>
-                    </li>
+                    @include('partials.messageNavBar')
                 @endif
 
                 @if(Auth::user()->teacher)
@@ -189,33 +139,7 @@
                         </a>
                     </li>
 
-                    <li class="nav-item nav-dropdown">
-                        <a href="#" class="nav-link nav-dropdown-toggle {{ Route::currentRouteName() == '#' ? 'active' : '' }}">
-                            <i class="fa fa-envelope"></i> Messages <i class="fa fa-caret-left"></i>
-                        </a>
-                        <ul class="nav-dropdown-items">
-                            <li class="nav-item">
-                                <a href="{{ route('message.inbox') }}" class="nav-link">
-                                    <i class="fa fa-inbox"></i> Inbox
-                                </a>
-                            </li>
-                            <li class="nav-item">
-                                <a href="{{ route('message.create') }}" class="nav-link">
-                                    <i class="fa fa-pencil"></i> Create
-                                </a>
-                            </li>
-                            <li class="nav-item">
-                                <a href="{{ route('message.sent') }}" class="nav-link">
-                                    <i class="fa fa-paper-plane"></i> Sent
-                                </a>
-                            </li>
-                            <li class="nav-item">
-                                <a href="{{ route('message.deleted') }}" class="nav-link">
-                                    <i class="fa fa-trash"></i> Deleted
-                                </a>
-                            </li>
-                        </ul>
-                    </li>
+                    @include('partials.messageNavBar')
 
                     <li class="nav-item nav-dropdown">
                         <a href="#" class="nav-link nav-dropdown-toggle {{ Route::currentRouteName() == '#' ? 'active' : '' }}">
