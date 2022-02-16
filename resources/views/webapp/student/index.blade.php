1ff457df18262cfa9bfaac4bfb88781629afd70b
Walter Prorok <w.prorok@ubreakifix.com>
2022-02-15 21:01:07 -0500

    MTA-41 UI updates to datetime and no text wrap class applied

--- resources/views/webapp/student/index.blade.php
+++ resources/views/webapp/student/index.blade.php
@@ -47,11 +47,11 @@
                             <td>{{ $student->phone_number }}</td>
                             <td>{{ $student->email }}</td>
                             <td>{{ $student->instrument }}</td>
-                            <th scope="row">
+                            <td class="text-nowrap">
                                 <a href="{{ route('student.profile', $student->id) }}" class="btn btn-sm btn-success" role="button" title="profile"><i class="fa fa-user"></i></a>
                                 <a href="{{ route('student.edit', $student->id) }}" class="btn btn-sm btn-primary" role="button" title="edit"><i class="fa fa-edit"></i></a>
                                 <a href="{{ route('student.schedule', $student->id) }}" class="btn btn-sm btn-warning" role="role" title="schedule"><i class="fa fa-calendar"></i></a>
-                            </th>
+                            </td>
                         </tr>
                     @empty
                         <tr>
