8064af80fe190320e57f7456a925e90aba534a0d
Walter Prorok <w.prorok@ubreakifix.com>
2022-05-03 16:03:16 -0400

    MTA-76 UI updates to login and register page

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -6,7 +6,7 @@
         <div class="row">
             <div class="col-xs-12 col-sm-6 col-sm-offset-2 col-md-6 col-md-offset-3">
                 <div class="section-header text-center">
-                    <h2 class="title">Sign Into Your Account</h2>
+                    <h2 class="title">Sign In To Your Account</h2>
                 </div>
                 <p class="text-center">Not a member yet? <a href="{{ route('pricing') }}"><u>Click here</u></a> to join our
                     community.</p>
@@ -16,7 +16,7 @@
                         <form class="form-horizontal" method="POST" action="{{ route('login') }}">
                             @csrf
                             <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
-                                <div class="col-md-8 col-md-offset-2">
+                                <div class="col-md-10 col-md-offset-1">
                                     <label for="email" class="control-label">Email Address</label>
                                     <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}" autofocus>
 
@@ -29,7 +29,7 @@
                             </div>
 
                             <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
-                                <div class="col-md-8 col-md-offset-2">
+                                <div class="col-md-10 col-md-offset-1">
                                     <label for="password" class="control-label">Password</label>
                                     <input id="password" type="password" class="form-control" name="password">
 
@@ -42,12 +42,12 @@
                             </div>
 
                             <div class="form-group">
-                                <div class="col-md-8 col-md-offset-2">
+                                <div class="col-md-10 col-md-offset-1">
                                     <div class="checkbox">
                                         <label>
                                             <input type="checkbox" name="remember" {{ old('remember') ? 'checked' : '' }}> Remember Me
                                         </label>
-                                        <a class="btn btn-link" href="{{ route('password.request') }}">
+                                        <a class="btn btn-link pull-right" href="{{ route('password.request') }}">
                                             Forgot Your Password?
                                         </a>
                                     </div>
@@ -55,7 +55,7 @@
                             </div>
 
                             <div class="form-group">
-                                <div class="col-md-8 col-md-offset-2">
+                                <div class="col-md-10 col-md-offset-1">
                                     <button type="submit" class="btn btn-primary btn-block">
                                         Login
                                     </button>
