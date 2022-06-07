0d83680dc40c3069a5c4339a2bed695f7c6a33bb
Walter Prorok <w.prorok@ubreakifix.com>
2022-06-07 09:13:47 -0400

    MTA-95 add contact resource and formatted time

---
+++ app/Http/Resources/ContactResource.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class ContactResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param Request $request
+     * @return array
+     */
+    public function toArray($request): array
+    {
+        return [
+            'id' => $this->id,
+            'name' => $this->name,
+            'email' => $this->email,
+            'subject' => $this->subject,
+            'message' => $this->message,
+            'created_at' => $this->created_at->format('Y-m-d H:i:s'),
+        ];
+    }
+}
