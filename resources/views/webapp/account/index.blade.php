97f8deae61ac00e127d7214c2da0fe8b4c9ca80b
Walter Prorok <w.prorok@ubreakifix.com>
2022-12-05 15:30:38 -0500

    MTA-174 ui fix for subscription inputs

--- resources/views/webapp/account/index.blade.php
+++ resources/views/webapp/account/index.blade.php
@@ -19,18 +19,22 @@
                     <div class="row">
                         <div class="col-md-6">
                             <div class="card-body">
-                                <label for="card-element">Amount</label>
-                                <select class="form-control" name="plan" id="sel1">
-                                    @foreach($plans as $plan)
-                                        <option value="{{ $plan->id }}">${{ number_format($plan->cost, 2) }} for {{ ucfirst($plan->name) }} Plan / {{ ucfirst($plan->slug) }}</option>
-                                    @endforeach
-                                </select>
-                                <label for="card-element">Credit Card</label>
-                                <div class="form-control" id="card-element">
-                                    <!-- A Stripe Element will be inserted here. -->
+                                <div class="form-group">
+                                    <label for="card-element">Amount 123</label>
+                                    <select class="form-control" name="plan" id="sel1">
+                                        @foreach($plans as $plan)
+                                            <option value="{{ $plan->id }}">${{ number_format($plan->cost, 2) }} for {{ ucfirst($plan->name) }} Plan / {{ ucfirst($plan->slug) }}</option>
+                                        @endforeach
+                                    </select>
                                 </div>
-                                <!-- Used to display form errors. -->
-                                <div id="card-errors" role="alert"></div>
+                                <div class="form-group">
+                                    <label for="card-element">Credit Card</label>
+                                    <div class="form-control" id="card-element">
+                                        <!-- A Stripe Element will be inserted here. -->
+                                    </div>
+                                    <!-- Used to display form errors. -->
+                                    <div id="card-errors" role="alert"></div>
+                                </div>
                             </div>
                         </div>
                         <div class="col-md-6">
