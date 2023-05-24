0a118a9860be3116eea1bda4d897ade8edb556d6
Walter Prorok <w.prorok@ubreakifix.com>
2023-05-23 20:13:12 -0400

    MTA-248 removed html tags from markup welcome email blade

--- resources/views/emails/teacher/welcome.blade.php
+++ resources/views/emails/teacher/welcome.blade.php
@@ -1,23 +1,23 @@
 @component('mail::message')
 # Welcome to Music Teachers Aid
 
-<p>Hey {{$user['first_name']}},</p>
+Hey {{$user['first_name']}},
 
-<p>We are excited that you signed up for Music Teachers Aid.</p>
+We are excited that you signed up for Music Teachers Aid.
 
-<p>Music Teachers Aid is built by a former music teacher and Berklee College of Music attendee.</p>
+Music Teachers Aid is built by a former music teacher and Berklee College of Music attendee.
 
-<p>We hope that you love our software as much as we do.</p>
+We hope that you love our software as much as we do.
 
-<p>If you have any questions or concerns please use the support link after you log in under your profile.</p>
+If you have any questions or concerns please use the support link after you log in under your profile.
 
 @component('mail::button', ['url' => route('contact')])
     Support
 @endcomponent
 
-<p>Your registered email is <b>{{$user['email']}}</b></p>
+Your registered email is **{{$user['email']}}**
 
-<p>To unlock the full potential of the software please sign up for a <a href="{{ route('account.subscription') }}" target="_blank">Premium Account</a> to unlock all the system features.</p>
+To unlock the full potential of the software please sign up for a <a href="{{ route('account.subscription') }}" target="_blank">Premium Account</a> to unlock all the system features.
 
 @component('mail::button', ['url' => route('account.subscription')])
 Get a Premium Account
