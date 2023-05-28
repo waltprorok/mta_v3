4a2be0f8bd078eb27dd1220536dbca8f09aef7f5
Walter Prorok <w.prorok@ubreakifix.com>
2023-05-28 09:17:01 -0400

    MTA-260 refactored student profile page to vuejs page

---
+++ resources/assets/js/components/students/Profile.vue
@@ -0,0 +1,36 @@
+<template src="./profile-template.html"></template>
+
+<script>
+import PhoneNumberFormat from "../PhoneNumberFormat";
+
+export default {
+    data() {
+        return {
+            profiles: [],
+        }
+    },
+
+    components: {
+        PhoneNumberFormat
+    },
+
+    mounted() {
+        this.fetchProfile();
+    },
+
+    methods: {
+        fetchProfile() {
+            let self = this;
+            let parameters = this.$route.fullPath
+            let id = parameters.split('/').pop()
+
+            axios.get('/students/' + id + '/profile')
+                .then((response) => {
+                    this.profiles = response.data;
+                }).catch((error) => {
+                console.log(error);
+            });
+        },
+    },
+}
+</script>
