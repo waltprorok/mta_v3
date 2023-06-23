39bc7421de16dfa0b044a94d7a225cddb5b3d51c
Walter Prorok <waltprorok@gmai.com>
2023-06-23 18:24:54 -0400

    MTA-280 added seeder to base class and registered package in config app

--- config/app.php
+++ config/app.php
@@ -220,6 +220,7 @@ return [
         'Log' => Illuminate\Support\Facades\Log::class,
         'Mail' => Illuminate\Support\Facades\Mail::class,
         'Markdown' => GrahamCampbell\Markdown\Facades\Markdown::class,
+        'NoCaptcha' => Anhskohbo\NoCaptcha\Facades\NoCaptcha::class,
         'Notification' => Illuminate\Support\Facades\Notification::class,
         'Password' => Illuminate\Support\Facades\Password::class,
         'Queue' => Illuminate\Support\Facades\Queue::class,
--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -14,6 +14,7 @@ class DatabaseSeeder extends Seeder
         $this->call(UsersTableDataSeeder::class);
         $this->call(PlanTableDataSeeder::class);
         $this->call(BlogTableDataSeeder::class);
+        $this->call(ContactTableDataSeeder::class);
         $this->call(StudentTableDataSeeder::class);
         $this->call(TeachersTableSeeder::class);
     }
