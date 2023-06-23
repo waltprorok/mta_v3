2205fce2d1496eb16ba9f1f6a26327768f6341ca
Walter Prorok <waltprorok@gmai.com>
2023-06-23 18:26:29 -0400

    MTA-282 added dark theme to captcha

--- resources/views/marketing/contact.blade.php
+++ resources/views/marketing/contact.blade.php
@@ -52,7 +52,7 @@
                         </div>
 
                         <div class="form-group {{ $errors->has('g-recaptcha-response') ? ' has-error' : '' }}">
-                            {!! app('captcha')->display() !!}
+                            {!! app('captcha')->display(['data-theme' => 'dark']) !!}
                             @if ($errors->has('g-recaptcha-response'))
                                 <span class="help-block">
                                         <strong>{{ $errors->first('g-recaptcha-response') }}</strong>
