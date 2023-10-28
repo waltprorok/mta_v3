00ee076d42ffe80895a9ec5101bf061ec3a473bb
Walter Prorok <waltprorok@gmai.com>
2023-10-28 08:36:07 -0400

    MTA-346 try catch around actions on new teacher record and email

--- app/Listeners/CreateTeacherListener.php
+++ app/Listeners/CreateTeacherListener.php
@@ -5,6 +5,8 @@ namespace App\Listeners;
 use App\Events\RegisterUserEvent;
 use App\Mail\WelcomeMail;
 use App\Models\Teacher;
+use Exception;
+use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Mail;
 
 class CreateTeacherListener
@@ -22,7 +24,7 @@ class CreateTeacherListener
     /**
      * Handle the event.
      *
-     * @param  RegisterUserEvent  $event
+     * @param RegisterUserEvent $event
      * @return void
      */
     public function handle(RegisterUserEvent $event)
@@ -30,13 +32,23 @@ class CreateTeacherListener
         $data = $event->getAttributes();
         $user = $event->getUser();
 
-        Teacher::query()->create([
-            'teacher_id' => $user->id,
-            'first_name' => $data['first_name'],
-            'last_name' => $data['last_name'],
-            'email' => $data['email'],
-        ]);
+        // create a new teacher record
+        try {
+            Teacher::query()->create([
+                'teacher_id' => $user->id,
+                'first_name' => $data['first_name'],
+                'last_name' => $data['last_name'],
+                'email' => $data['email'],
+            ]);
+        } catch (Exception $exception) {
+            Log::error($exception->getMessage());
+        }
 
-        Mail::to($data['email'])->send(new WelcomeMail($user));
+        // email new user
+        try {
+            Mail::to($data['email'])->send(new WelcomeMail($user));
+        } catch (Exception $exception) {
+            Log::warning($exception->getMessage());
+        }
     }
 }
