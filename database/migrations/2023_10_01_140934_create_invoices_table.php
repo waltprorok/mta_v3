6c059fb13da860d7b888fffc8fb8f6134bd1724f
Walter Prorok <waltprorok@gmai.com>
2023-11-18 09:00:56 -0500

    MTA-349 correct migration invoice

--- database/migrations/2023_10_01_140934_create_invoices_table.php
+++ database/migrations/2023_10_01_140934_create_invoices_table.php
@@ -27,9 +27,9 @@ class CreateInvoicesTable extends Migration
             $table->decimal('total')->default(0.0);
             $table->decimal('balance_due')->default(0.0);
             $table->decimal('payment')->default(0.0);
-            $table->integer('payment_type_id')->unsigned()->nullable();
-            $table->foreign('payment_type_id')->references('id')->on('payment_types');
             $table->decimal('adjustments')->default(0.0);
+            $table->unsignedBigInteger('payment_type_id');
+            $table->foreign('payment_type_id')->references('id')->on('payment_types');
             $table->date('due_date')->nullable();
             $table->softDeletes();
             $table->timestamps();
