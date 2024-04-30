f2688ed410c545387f83ff6af0834ca5739b8d96
Walter Prorok <waltprorok@gmai.com>
2024-04-30 15:00:30 -0400

    MTA-399 changed variable name from update to blog on edit template

--- resources/views/webapp/admin/blog/edit.blade.php
+++ resources/views/webapp/admin/blog/edit.blade.php
@@ -7,14 +7,14 @@
         <br>
         <div class="card">
             <div class="card-body">
-                <form method="POST" action="{{ route('admin.blog.update', $update->id) }}" enctype="multipart/form-data">
+                <form method="POST" action="{{ route('admin.blog.update', $blog->id) }}" enctype="multipart/form-data">
                     @method('PUT')
                     @csrf
                     <div class="row">
                         <div class="col-sm-6">
                             <div class="form-group{{ $errors->has('title') ? ' has-error' : '' }}">
                                 <label for="title">Title</label>
-                                <input type="text" class="form-control" name="title" value="{{ $update->title }}">
+                                <input type="text" class="form-control" name="title" value="{{ $blog->title }}">
 
                                 @if ($errors->has('title'))
                                     <span class="help-block">
@@ -27,7 +27,7 @@
                         <div class="col-sm-6">
                             <div class="form-group{{ $errors->has('slug') ? ' has-error' : '' }}">
                                 <label for="slug">Slug</label>
-                                <input type="text" class="form-control" name="slug" value="{{ $update->slug }}">
+                                <input type="text" class="form-control" name="slug" value="{{ $blog->slug }}">
 
                                 @if ($errors->has('slug'))
                                     <span class="help-block">
@@ -42,7 +42,7 @@
                         <div class="col-sm-6">
                             <div class="form-group{{ $errors->has('body') ? ' has-error' : '' }}">
                                 <label for="body">Body</label>&nbsp;<small>*Markdown Available</small>
-                                <textarea class="form-control" name="body" rows="24">{{ $update->body }}</textarea>
+                                <textarea class="form-control" name="body" rows="24">{{ $blog->body }}</textarea>
 
                                 @if ($errors->has('body'))
                                     <span class="help-block">
@@ -55,11 +55,11 @@
                         <div class="col-sm-6">
                             <div class="form-group">
                                 <label for="image">Featured Image</label>
-                                @if ($update->image_url)
+                                @if ($blog->image_url)
                                     <div class="text-center">
-                                        <img src="{{ $update->image_url }}" alt="{{ $update->image }}" width="100%">
+                                        <img src="{{ $blog->image_url }}" alt="{{ $blog->image }}" width="100%">
                                     </div>
-                                    <input type="hidden" name="updateImage" value="{{ $update->image }}">
+                                    <input type="hidden" name="updateImage" value="{{ $blog->image }}">
                                 @else
                                     <div class="text-center">
                                         <p>There is no image for this news article.</p>
@@ -74,7 +74,7 @@
                             <div class="form-group{{ $errors->has('released_on') ? ' has-error' : '' }}">
                                 <label for="released_on">Release Date</label>
                                 <input type="text" class="form-control" name="released_on" id="blogRelease"
-                                       autocomplete="off" value="{{ date('Y-m-d', strtotime($update->released_on)) }}">
+                                       autocomplete="off" value="{{ date('Y-m-d', strtotime($blog->released_on)) }}">
 
                                 @if ($errors->has('released_on'))
                                     <span class="help-block">
@@ -88,7 +88,7 @@
                             <div class="form-group{{ $errors->has('release_time') ? ' has-error' : '' }}">
                                 <label for="start_time" class="control-label">Release Time</label>
                                 <select class="form-control" id="release_time" name="release_time">
-                                    <option value="{{ $update->date_blog_raw }}">{{ $update->date_hour_min }}</option>
+                                    <option value="{{ $blog->date_blog_raw }}">{{ $blog->date_hour_min }}</option>
                                     <option value="09:00:00">9:00 AM</option>
                                     <option value="12:00:00">12:00 PM</option>
                                     <option value="15:00:00">3:00 PM</option>
