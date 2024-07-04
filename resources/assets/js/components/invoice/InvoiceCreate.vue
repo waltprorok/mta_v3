0b1cf10be8bcd4f33371c0e3f8941cfc9bc59a73
Walter Prorok <waltprorok@gmai.com>
2024-07-03 20:26:10 -0400

    MTA-423 WIP: added table and date formatting

--- resources/assets/js/components/invoice/InvoiceCreate.vue
+++ resources/assets/js/components/invoice/InvoiceCreate.vue
@@ -2,13 +2,13 @@
     <div class="card">
         <div class="form-control">
             <form action="#" @submit.prevent>
-                <div class="row pl-5 pt-2">
+                <div class="row pl-4 pt-2">
                     <div class="col-md-4">
                         <div class="form-group">
                             <select id="single-select" class="form-control" @change="getSelectedStudent($event)">
                                 <option>-- Select Student --</option>
-                                <option v-for="data in list" :value="data.student_id" :key="data.id">
-                                    {{ data.first_name }} {{ data.last_name }}
+                                <option v-for="row in list" :value="row.student_id" :key="row.id">
+                                    {{ row.first_name }} {{ row.last_name }}
                                 </option>
                             </select>
                         </div>
@@ -16,7 +16,7 @@
                 </div>
 
                 <div v-show="selected">
-                    <div class="row p-5">
+                    <div class="row pl-4">
                         <div class="col-md-6 text-left" v-if="selected">
                             <p class="font-weight-bold mb-4">Teacher Information</p>
                             <p class="mb-1"><span class="text-muted"></span><strong>{{ student.student_teacher.studio_name }}</strong></p>
@@ -33,7 +33,7 @@
 
                     <hr class="my-1">
 
-                    <div class="row p-5 pb-1">
+                    <div class="row p-4 pb-1">
                         <div class="col-md-6">
                             <p class="font-weight-bold mb-4">Student Information</p>
                             <p class="mb-1">{{ student.first_name }} {{ student.last_name }}</p>
@@ -46,80 +46,83 @@
                         </div>
                     </div>
 
-                    <div class="row pl-5">
-                        <div class="col-md-2">
-                            <div class="form-group">
-                                <label for="normal-input" class="form-control-label">Lesson Start Date</label>
-                                <input id="normal-input" class="form-control" v-for="lesson in student.lessons" v-model.trim="lesson.start_date" type="text" readonly>
-                            </div>
-                        </div>
-
-                        <div class="col-md-2">
-                            <div class="form-group">
-                                <label for="normal-input" class="form-control-label">Lesson End Date</label>
-                                <input id="normal-input" class="form-control" v-for="lesson in student.lessons" v-model.trim="lesson.end_date" type="text" readonly>
-                            </div>
-                        </div>
-
-                        <div class="col-md-2">
-                            <div class="form-group">
-                                <label for="normal-input" class="form-control-label">Completed</label>
-                                <select id="single-select" class="form-control" v-for="lesson in student.lessons" @change="updateLesson(lesson.id, $event)">
-                                    <option :value="lesson.complete" v-model="lesson.complete">
-                                        {{ lesson.complete ? "Yes" : "No" }}
-                                    </option>
-                                    <option :value="!lesson.complete" v-model="!lesson.complete">
-                                        {{ lesson.complete ? "No" : "Yes" }}
-                                    </option>
-                                </select>
-                            </div>
-                        </div>
-
-                        <div class="col-md-2">
-                            <div class="form-group">
-                                <label for="normal-input" class="form-control-label">Type</label>
-                                <input id="normal-input" class="form-control" v-for="lesson in student.lessons" v-model.trim="lesson.billing_rate.description" type="text" readonly>
-                            </div>
-                        </div>
-
-                        <div class="col-md-2">
-                            <div class="form-group">
-                                <label for="normal-input" class="form-control-label">Amount</label>
-                                <input id="normal-input" class="form-control" v-for="lesson in student.lessons" v-model.trim="lesson.billing_rate.amount" type="text" readonly>
-                            </div>
+                    <div class="row pl-4">
+                        <div class="col-md-12">
+                            <table class="table">
+                                <thead>
+                                <tr>
+                                    <th class="border-0 text-uppercase small font-weight-bold">Lesson Start Date</th>
+                                    <th class="border-0 text-uppercase small font-weight-bold">Lesson End Date</th>
+                                    <th class="border-0 text-uppercase small font-weight-bold">Completed</th>
+                                    <th class="border-0 text-uppercase small font-weight-bold">Rate Type</th>
+                                    <th class="border-0 text-uppercase small font-weight-bold">Amount</th>
+                                </tr>
+                                </thead>
+                                <tbody>
+                                <tr v-for="lesson in student.lessons">
+                                    <td>{{ lesson.start_date | dateParse('YYYY-MM-DD HH:mm:ss') | dateFormat('MM-DD-YYYY h:mm a') }}</td>
+                                    <td>{{ lesson.end_date | dateParse('YYYY-MM-DD HH:mm:ss') | dateFormat('MM-DD-YYYY h:mm a') }}</td>
+                                    <td>
+                                        <select id="single-select" class="form-control" @change="updateLesson(lesson.id, $event)">
+                                            <option :value="lesson.complete" v-model="lesson.complete">
+                                                {{ lesson.complete ? "Yes" : "No" }}
+                                            </option>
+                                            <option :value="!lesson.complete" v-model="!lesson.complete">
+                                                {{ lesson.complete ? "No" : "Yes" }}
+                                            </option>
+                                        </select>
+                                    </td>
+                                    <td>{{ lesson.billing_rate.description }}</td>
+                                    <td>{{ lesson.billing_rate.amount | toCurrency }}</td>
+                                </tr>
+                                </tbody>
+                            </table>
                         </div>
                     </div>
 
-                    <div class="row pl-5">
+                    <div class="row pl-4 pt-2 pb-4">
                         <div class="col-md-3">
                             <div class="form-group">
                                 <label for="normal-input" class="form-control-label">Due Date</label>
-                                <input id="normal-input" class="form-control" v-model.trim="invoice.due_date" type="date">
+                                <input id="normal-input"
+                                       class="form-control"
+                                       v-model.trim="invoice.due_date"
+                                       type="date"
+                                       v-on:keydown.enter.prevent>
                             </div>
                         </div>
 
                         <div class="col-md-3">
                             <div class="form-group">
-                                <div class="form-group">
-                                    <label for="normal-input" class="form-control-label">Send to Email</label>
-                                    <input id="normal-input" class="form-control" v-model.trim="student.additional_email" type="email" placeholder="Send to this email">
+                                <label for="normal-input" class="form-control-label">Send to Email</label>
+                                <div class="input-group mb-3">
+                                    <input id="normal-input"
+                                           class="form-control"
+                                           v-model.trim="student.additional_email"
+                                           type="email"
+                                           placeholder="Send additional copy to this email"
+                                           v-on:keydown.enter.prevent>
+                                    <div class="input-group-append">
+                                        <span class="input-group-text"><i class="fa fa-envelope"></i></span>
+                                    </div>
                                 </div>
                             </div>
                         </div>
                     </div>
 
-                    <div class="row d-flex flex-row bg-dark text-white pl-5 pt-3 pb-2">
+                    <div class="row d-flex flex-row bg-dark text-white p-4">
                         <div class="col-md-3">
                             <div class="form-group">
-                                <label for="normal-input" class="form-control-label">Discount</label>
+                                <label for="discount" class="form-control-label">Discount</label>
                                 <div class="input-group mb-3">
-                                    <input id="normal-input"
+                                    <input id="discount"
                                            class="form-control"
                                            v-model="invoice.discount"
                                            type="number"
                                            min="0"
                                            max="100"
-                                           @input="updateTotal()">
+                                           @input="updateTotal()"
+                                           v-on:keydown.enter.prevent>
                                     <div class="input-group-append">
                                         <span class="input-group-text">%</span>
                                     </div>
@@ -127,33 +130,31 @@
                             </div>
                         </div>
 
-                        <div class="col-md-2">
+                        <div class="col-md-3">
                             <div class="form-group">
-                                <label for="normal-input" class="form-control-label">Sub Total</label>
-                                <p class="form-control-plaintext">{{ invoice.subtotal | toCurrency }}</p>
+                                <label for="subtotal" class="form-control-label">Sub Total</label>
+                                <p class="form-control-plaintext h4">{{ invoice.subtotal | toCurrency }}</p>
                             </div>
                         </div>
 
                         <div class="col-md-3">
                             <div class="form-group">
-                                <label for="normal-input" class="form-control-label">Total</label>
-                                <p class="form-control-plaintext">{{ invoice.total | toCurrency }}</p>
-                                <!--                                <input id="normal-input" class="form-control" v-model.number="invoice.total" type="number" readonly>-->
+                                <label for="total" class="form-control-label">Total</label>
+                                <p class="form-control-plaintext h4">{{ invoice.total | toCurrency }}</p>
                             </div>
                         </div>
 
                         <div class="col-md-3">
                             <div class="form-group">
-                                <label for="normal-input" class="form-control-label">Balance Due</label>
-                                <p class="form-control-plaintext">{{ invoice.balance_due | toCurrency }}</p>
-                                <!--                                <input id="normal-input" class="form-control" v-model.number="invoice.balance_due" type="number" readonly>-->
+                                <label for="balance-due" class="form-control-label">Balance Due</label>
+                                <p class="form-control-plaintext h4">{{ invoice.balance_due | toCurrency }}</p>
                             </div>
                         </div>
                     </div>
 
-                    <div class="form-group pull-right pt-4 pr-5">
+                    <div class="form-group pull-right pt-4 pr-md-4">
                         <button @click="clearForm()" class="btn btn-default">Cancel</button>
-                        <button @click="createInvoice()" type="submit" class="btn btn-primary">Save</button>
+                        <button @click="createInvoice()" class="btn btn-primary" type="submit">Save</button>
                     </div>
                 </div>
             </form>
@@ -164,9 +165,6 @@
 
 
 <script>
-// import {dateParse} from "@vuejs-community/vue-filter-date-parse";
-// import {dateFormat} from "vue-filter-date-format";
-
 import PhoneNumberFormat from "../PhoneNumberFormat.vue";
 
 export default {
@@ -261,20 +259,18 @@ export default {
         updateTotal: function () {
             let subTotal = this.invoice.subtotal;
             let discount = subTotal * (this.invoice.discount / 100);
-            // let balanceDue = this.invoice.balance_due;
-
             this.invoice.total = subTotal - discount;
             this.invoice.balance_due = subTotal - discount;
-            // $subTotal * ($discount / 100)
         },
 
         calculateTotal: function () {
+            // TODO: need logic for different billing rate situations
             this.student.lessons.map((i) => {
-                this.invoice.balance_due = i.billing_rate.amount;
+                this.invoice.discount = 0;
                 this.invoice.total = i.billing_rate.amount;
                 this.invoice.subtotal = i.billing_rate.amount;
+                this.invoice.balance_due = i.billing_rate.amount;
             });
-
         },
 
         clearForm: function () {
