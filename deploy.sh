8b4557e019743a9153c8996820cf3877b3d1c025
Walter Prorok <waltprorok@gmai.com>
2024-09-16 12:27:17 -0400

    MTA-471 updates to deploy script

--- deploy.sh
+++ deploy.sh
@@ -3,14 +3,15 @@ php artisan down
 git reset --hard
 git pull
 php artisan migrate
+export COMPOSER_ALLOW_SUPERUSER=1
 composer install --prefer-dist --optimize-autoloader --no-dev -o --no-interaction
 php artisan optimize:clear
+npm install
+npm run production
 php artisan event:clear
 php artisan route:cache
 php artisan view:cache
 php artisan event:cache
 php artisan config:cache
-npm install
-npm run production
 php artisan queue:restart
 php artisan up
