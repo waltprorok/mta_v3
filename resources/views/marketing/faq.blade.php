81d1c0c3af30aaabc6432bc2a77d21ddb7754812
Walter Prorok <waltprorok@gmai.com>
2024-10-04 21:35:30 -0400

    MTA-506 added faq

--- resources/views/marketing/faq.blade.php
+++ resources/views/marketing/faq.blade.php
@@ -24,9 +24,50 @@
                             </div>
                             <div id="collapseOne" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingOne">
                                 <div class="panel-body">
-                                    Music Teachers Aid is software that aids private music teachers.
-                                    The software keeps track of students, lessons, payments, give feedback to
-                                    parents, and professional invoicing.
+                                    <p>Music Teachers Aid is software that aids private music teachers.</p>
+                                    <p>The software keeps track of students, lessons, payments, give feedback to
+                                    parents, and professional invoicing.</p>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="panel panel-default">
+                            <div class="panel-heading" role="tab" id="headingFour">
+                                <h4 class="panel-title">
+                                    <a role="button" data-toggle="collapse" data-parent="#accordion"
+                                       href="#collapseFour"
+                                       aria-expanded="true" aria-controls="collapseFour">
+                                        <i class="more-less glyphicon glyphicon-plus"></i>
+                                        Why do I need software to run my music teaching studio?
+                                    </a>
+                                </h4>
+                            </div>
+                            <div id="collapseFour" class="panel-collapse collapse" role="tabpanel"
+                                 aria-labelledby="headingFour">
+                                <div class="panel-body">
+                                    <p>This is a great question. Two words, Administrative Work. Keeping tracking of
+                                    students, scheduling, rescheduling, and getting paid is the bottom line.</p>
+                                    <p>We want that experience to be as pleasant as possible for you the instructor and the
+                                    student. We want to help take your level of professionalism up a notch.</p>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="panel panel-default">
+                            <div class="panel-heading" role="tab" id="headingFive">
+                                <h4 class="panel-title">
+                                    <a role="button" data-toggle="collapse" data-parent="#accordion" href="#collapseFive"
+                                       aria-expanded="true" aria-controls="collapseFive">
+                                        <i class="more-less glyphicon glyphicon-plus"></i>
+                                        Are there limits to the number of clients or appointments?
+                                    </a>
+                                </h4>
+                            </div>
+
+                            <div id="collapseFive" class="panel-collapse collapse" role="tabpanel"
+                                 aria-labelledby="headingFive">
+                                <div class="panel-body">
+                                    Nope, Music Teachers Aid is the same price no matter how many clients or appointments you schedule.
                                 </div>
                             </div>
                         </div>
@@ -69,35 +110,14 @@
                             <div id="collapseThree" class="panel-collapse collapse" role="tabpanel"
                                  aria-labelledby="headingThree">
                                 <div class="panel-body">
-                                    Yes of course you can cancel at anytime, though we hope you don't ever have to. The
-                                    subscription process grants 30 days per payment cycle. If you signed on the 1st of
-                                    the month and you cancel on the 15th your subscription is valid to the 30th of that
-                                    month. We grant a FREE 30 day trial period to test out the software.
+                                    <p>Yes of course you can cancel at anytime, though we hope you don't ever have to. The
+                                    subscription process grants 30 days per payment cycle.</p>
+                                    <p>If you signed on the 1st of the month and you cancel on the 15th your subscription
+                                        is valid to the 30th of that month. We grant a FREE 30 day trial period to test out the software.</p>
                                 </div>
                             </div>
                         </div>
 
-                        <div class="panel panel-default">
-                            <div class="panel-heading" role="tab" id="headingFour">
-                                <h4 class="panel-title">
-                                    <a role="button" data-toggle="collapse" data-parent="#accordion"
-                                       href="#collapseFour"
-                                       aria-expanded="true" aria-controls="collapseFour">
-                                        <i class="more-less glyphicon glyphicon-plus"></i>
-                                        Why do I need software to run my music studio?
-                                    </a>
-                                </h4>
-                            </div>
-                            <div id="collapseFour" class="panel-collapse collapse" role="tabpanel"
-                                 aria-labelledby="headingFour">
-                                <div class="panel-body">
-                                    This is a great question. Two words, Administrative Work. Keeping tracking of
-                                    students, scheduling, rescheduling, and getting paid is the bottom line. We want
-                                    that experience to be as pleasant as possible for you the instructor and the
-                                    student. We want to help take your level of professionalism up a notch.
-                                </div>
-                            </div>
-                        </div>
                     </div>
                 </div>
             </div>
