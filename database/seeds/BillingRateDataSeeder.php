e30c78b976da1c8d0959ce78540fa34d6e4b899a
Walter Prorok <waltprorok@gmai.com>
2024-10-18 17:25:37 -0400

    MTA-535 added billing rate data seeder

---
+++ database/seeds/BillingRateDataSeeder.php
@@ -0,0 +1,52 @@
+<?php
+
+use App\Models\BillingRate;
+use Carbon\Carbon;
+use Illuminate\Database\Seeder;
+
+class BillingRateDataSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        BillingRate::query()->insert([
+            [
+                'id' => '1',
+                'teacher_id' => '3',
+                'type' => 'monthly',
+                'amount' => '100',
+                'description' => 'Monthly Rate',
+                'default' => '1',
+                'active' => '1',
+                'created_at' => Carbon::now(),
+                'updated_at' => Carbon::now(),
+            ],
+            [
+                'id' => '2',
+                'teacher_id' => '3',
+                'type' => 'hourly',
+                'amount' => '50',
+                'description' => 'Hourly Rate',
+                'default' => '0',
+                'active' => '1',
+                'created_at' => Carbon::now(),
+                'updated_at' => Carbon::now(),
+            ],
+            [
+                'id' => '3',
+                'teacher_id' => '3',
+                'type' => 'lesson',
+                'amount' => '25',
+                'description' => 'Per Lesson',
+                'default' => '0',
+                'active' => '1',
+                'created_at' => Carbon::now(),
+                'updated_at' => Carbon::now(),
+            ],
+        ]);
+    }
+}
