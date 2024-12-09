aea37fb086e99afc31e9a81c3a3b61977d664cce
Walter Prorok <waltprorok@gmai.com>
2024-12-09 11:33:32 -0500

    MTA-650 default true auto schedule new active student teacher setting

--- resources/assets/js/components/settings/Settings.vue
+++ resources/assets/js/components/settings/Settings.vue
@@ -64,7 +64,7 @@ export default {
                 'calendar': 'month',
                 'calendar_min_time': '08:00:00',
                 'calendar_max_time': '22:00:00',
-                'auto_schedule_new_active_students': false,
+                'auto_schedule_new_active_students': true,
             },
             calendar: [
                 {value: 'month', name: 'Month'},
