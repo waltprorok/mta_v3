11f3e23e6fcc2a111c4e803990a574bab07d3b5a
Walter Prorok <waltprorok@gmai.com>
2024-12-10 12:43:37 -0500

    MTA-659 added unit tests

---
+++ tests/Feature/Http/Requests/StoreSupportRequestTest.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace Tests\Feature\Http\Requests;
+
+use App\Http\Requests\StoreSupportRequest;
+use App\Models\User;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Support\Facades\Validator;
+use Tests\TestCase;
+
+class StoreSupportRequestTest extends TestCase
+{
+    use WithFaker;
+
+    public $request;
+
+    public function setUp(): void
+    {
+        parent::setUp();
+        $this->request = new StoreSupportRequest();
+    }
+
+    public function test_verifies_authorized()
+    {
+        $user = factory(User::class)->make(["teacher" => true]);
+
+        $this->actingAs($user)->assertTrue($this->request->authorize());
+    }
+
+    public function test_request_pass()
+    {
+        $validator = Validator::make([
+            'name' => $this->faker->name,
+            'email' => $this->faker->unique()->safeEmail,
+            'subject' => $this->faker->sentence(),
+            'message' => $this->faker->sentences,
+        ], $this->request->rules());
+
+        $this->assertTrue($validator->passes());
+    }
+
+    /**
+     * @return array[]
+     */
+    public function requestDataProvider(): array
+    {
+        return [
+            'Not Email fail' =>
+                ['email', 'john.snow'],
+            'Email null fail' =>
+                ['email', null],
+            'Email min fail' =>
+                ['email', 'ab'],
+            'Subject min fail' =>
+                ['subject', 'ab'],
+            'Subject null fail' =>
+                ['subject', null],
+            'Message min fail' =>
+                ['message', 'ab'],
+            'Message null fail' =>
+                ['message', null],
+        ];
+    }
+
+    /**
+     * @dataProvider requestDataProvider
+     */
+    public function test_request_fail($key, $value)
+    {
+        $validator = Validator::make([
+            $key => $value,
+        ], $this->request->rules());
+
+        $this->assertFalse($validator->passes());
+        $this->assertContains($key, $validator->errors()->keys());
+    }
+}
