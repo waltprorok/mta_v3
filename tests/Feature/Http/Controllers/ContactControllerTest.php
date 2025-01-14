98b19cb13407515f614ef01cd61211dcc7ced4fa
Walter Prorok <waltprorok@gmail.com>
2025-01-14 16:31:38 -0500

    MTA-720 adding unit tests for contact controller

--- tests/Feature/Http/Controllers/ContactControllerTest.php
+++ tests/Feature/Http/Controllers/ContactControllerTest.php
@@ -26,7 +26,7 @@ class ContactControllerTest extends TestCase
             ->assertViewIs('webapp.admin.contact.index');
     }
 
-    public function test_contact_index_route_200()
+    public function test_contact_index_url_200()
     {
         $user = factory(User::class)->create(['admin' => 1]);
 
@@ -35,10 +35,49 @@ class ContactControllerTest extends TestCase
         $response->assertOk();
     }
 
-    public function test_contacts_is_not_null()
+    public function test_contact_factory()
     {
-        $contacts = factory(Contact::class, 5)->make();
+        factory(Contact::class, 5)->create();
 
-        $this->assertNotNull($contacts);
+        $this->assertDatabaseCount('contacts', 5);
+    }
+
+    public function test_contact_show()
+    {
+        $user = factory(User::class)->create(['admin' => 1]);
+
+        $contact = factory(Contact::class)->create();
+
+        $response = $this->actingAs($user)->get('/web/contacts/' . $contact->id);
+
+        $response->assertOk();
+        $response->assertJsonCount(9);
+    }
+
+    public function test_contact_create_success()
+    {
+        $user = factory(User::class)->create(['admin' => 1]);
+
+        $this->actingAs($user)->post('/web/contacts', [
+            'name' => 'Test Name',
+            'email' => 'test@domain.com',
+            'subject' => 'Test Subject',
+            'message' => 'Test Message',
+        ]);
+
+        $this->assertDatabaseCount('contacts', 1);
+    }
+
+    public function test_contact_delete_success()
+    {
+        $user = factory(User::class)->create(['admin' => 1]);
+
+        $contact = factory(Contact::class)->create();
+
+        $response = $this->actingAs($user)->delete('/web/contacts/' . $contact->id);
+
+        $response->assertOk();
+
+        $this->assertSoftDeleted('contacts', $contact->toArray());
     }
 }
