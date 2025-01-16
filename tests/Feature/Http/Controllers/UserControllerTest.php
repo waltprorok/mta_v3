3dacbc38aa888c952cb8d59530f5161131d06e18
Walter Prorok <waltprorok@gmail.com>
2025-01-16 16:18:01 -0500

    MTA-727 created user controller test

---
+++ tests/Feature/Http/Controllers/UserControllerTest.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace Feature\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+
+class UserControllerTest extends TestCase
+{
+    use RefreshDatabase;
+
+    public $user;
+
+    public function setUp(): void
+    {
+        parent::setUp();
+        $this->user = factory(User::class)->create(['admin' => true, 'student' => false]);
+    }
+
+    public function test_user_index_view_200()
+    {
+        $response = $this->actingAs($this->user)->get(route('admin.users'));
+
+        $response->assertOk()
+            ->assertViewIs('webapp.admin.user.index');
+    }
+
+    public function test_admin_user_index_url_200()
+    {
+        $response = $this->actingAs($this->user)->get('/admin/users');
+
+        $response->assertOk()
+            ->assertViewIs('webapp.admin.user.index');
+    }
+
+    public function test_user_index_web_url_200()
+    {
+        $response = $this->actingAs($this->user)->get('/web/users');
+
+        $response->assertOk();
+    }
+
+    public function test_user_factory()
+    {
+        factory(User::class, 5)->create(['teacher' => true, 'student' => true]);
+
+        $this->assertDatabaseCount('users', 6);
+    }
+}
