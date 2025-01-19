94e8dea157e0a499f16966a68fa2fd5c1e29d734
Walter Prorok <waltprorok@gmail.com>
2025-01-19 09:32:57 -0500

    MTA-734 added tests for student list controller

--- tests/Feature/Http/Controllers/StudentListControllerTest.php
+++ tests/Feature/Http/Controllers/StudentListControllerTest.php
@@ -2,6 +2,7 @@
 
 namespace Tests\Feature\Http\Controllers;
 
+use App\Models\Student;
 use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
@@ -15,7 +16,7 @@ class StudentListControllerTest extends TestCase
     public function setUp(): void
     {
         parent::setUp();
-        $this->user = factory(User::class)->create(['teacher' => true, 'student' => false]);
+        $this->user = factory(User::class)->create(['id' => 3, 'teacher' => true, 'student' => false]);
     }
 
     public function test_student_list_index_view_200()
@@ -32,5 +33,52 @@ class StudentListControllerTest extends TestCase
 
         $response->assertOk()
             ->assertViewIs('webapp.student.index');
+    }
+
+    public function test_student_factory()
+    {
+        factory(Student::class, 5)->create();
+
+        $this->assertDatabaseCount('students', 5);
+    }
+
+    public function test_student_list_active_200()
+    {
+        factory(Student::class)->create(['teacher_id' => $this->user->id, 'status' => 1]);
+
+        $response = $this->actingAs($this->user)->get('/web/active');
+
+        $response->assertOk();
+        $response->assertJsonCount(1);
+    }
+
+    public function test_student_list_waitlist_200()
+    {
+        factory(Student::class)->create(['teacher_id' => $this->user->id, 'status' => 2]);
+
+        $response = $this->actingAs($this->user)->get('/web/waitlist');
+
+        $response->assertOk();
+        $response->assertJsonCount(1);
+    }
+
+    public function test_student_list_lead_200()
+    {
+        factory(Student::class)->create(['teacher_id' => $this->user->id, 'status' => 3]);
+
+        $response = $this->actingAs($this->user)->get('/web/leads');
+
+        $response->assertOk();
+        $response->assertJsonCount(1);
+    }
+
+    public function test_student_list_inactive_200()
+    {
+        factory(Student::class)->create(['teacher_id' => $this->user->id, 'status' => 4]);
+
+        $response = $this->actingAs($this->user)->get('/web/inactive');
+
+        $response->assertOk();
+        $response->assertJsonCount(1);
     }
 }
