c32e204b522d574d3ee7db4a73b697f57aac9352
Walter Prorok <waltprorok@gmail.com>
2025-02-06 16:27:24 -0500

    MTA-792 laravel upgrade from version 8 to 9

--- database/migrations/2025_02_06_161718_change_teacher_settings_table.php
+++ database/migrations/2025_02_06_161718_change_teacher_settings_table.php
@@ -4,8 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      *
@@ -25,6 +24,8 @@ return new class extends Migration
      */
     public function down()
     {
-        //
+        Schema::table('teacher_settings', function (Blueprint $table) {
+            $table->enum('calendar', ['month', 'listWeek', 'agendaWeek', 'agendaDay'])->nullable()->default('month')->change();
+        });
     }
 };
