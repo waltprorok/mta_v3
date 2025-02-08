679b2c42cd1156a1db4f3ed3ebd520b9c516f35f
Walter Prorok <waltprorok@gmail.com>
2025-02-08 08:13:42 -0500

    MTA-797 null safety check on adding student

--- app/Http/Controllers/StudentController.php
+++ app/Http/Controllers/StudentController.php
@@ -192,7 +192,7 @@ class StudentController extends Controller
 
     private function isStatusActive($request): bool
     {
-        return Auth::user()->teacherSetting->auto_schedule_new_active_students && $request->get('status') == Student::ACTIVE;
+        return Auth::user()?->teacherSetting?->auto_schedule_new_active_students && $request->get('status') == Student::ACTIVE;
     }
 
     /**
