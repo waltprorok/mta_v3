1d17e343c1a3e04677d57731ee9d5c72ab1f956d
Walter Prorok <waltprorok@gmail.com>
2025-02-12 15:30:54 -0500

    MTA-807 created components for invoices

---
+++ resources/assets/js/components/invoice/components/InvoicePaidTiles.vue
@@ -0,0 +1,43 @@
+<template>
+    <div class="row">
+        <div class="col-md-3">
+            <div class="card p-4">
+                <div class="card-body d-flex justify-content-between align-items-center">
+                    <div>
+                        <span class="h4 d-block font-weight-normal mb-2">{{ paid }}</span>
+                        <span class="font-weight-light">Paid</span>
+                    </div>
+
+                    <div class="h2 text-muted">
+                        <i class="fa fa-money" aria-hidden="true"></i>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-3">
+            <div class="card p-4">
+                <div class="card-body d-flex justify-content-between align-items-center">
+                    <div>
+                        <span class="h4 d-block font-weight-normal mb-2">{{ notPaid }}</span>
+                        <span class="font-weight-light">Not Paid</span>
+                    </div>
+
+                    <div class="h2 text-muted">
+                        <i class="fa fa-bank" aria-hidden="true"></i>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'InvoicePaidTiles',
+    props: {
+        paid: Number,
+        notPaid: Number,
+    },
+
+}
+</script>
