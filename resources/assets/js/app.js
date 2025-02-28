1ecaa391a3b3914424df0dfda3f96cec2906f676
Walter Prorok <waltprorok@gmail.com>
2025-02-28 14:40:48 -0500

    MTA-842 admin edit user page added

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -76,7 +76,8 @@ Vue.component('students', require('./components/admin/student/Students.vue').def
 Vue.component('support', require('./components/admin/support/Support.vue').default);
 Vue.component('studentList', require('./components/student/list/StudentList.vue').default);
 Vue.component('teachers', require('./components/admin/teacher/Teachers.vue').default);
-Vue.component('users', require('./components/admin/user/Users.vue').default);
+Vue.component('users', require('./components/admin/user/list/Users.vue').default);
+Vue.component('userEdit', require('./components/admin/user/edit/UserEdit.vue').default);
 Vue.component('reportCompletedLessonsLine', require('./components/reports/CompletedLessonsLine.vue').default);
 Vue.component('reportStudentStatusBar', require('./components/reports/StudentStatusBar.vue').default);
 Vue.component('reportInvoicePaymentsBar', require('./components/reports/InvoicePaymentsBar.vue').default);
---
+++ resources/views/webapp/admin/user/edit.blade.php
@@ -0,0 +1,18 @@
+@extends('layouts.webapp')
+@section('title', 'Admin Edit User')
+@section('content')
+
+    <div class="col-12">
+        <h4>Edit User</h4>
+        <ul class="breadcrumb">
+            <li class="breadcrumb-item"></li>
+        </ul>
+
+        <div id="app">
+            <user-edit></user-edit>
+        </div>
+    </div>
+
+    <script src="{{ mix('js/app.js') }}"></script>
+
+@endsection
