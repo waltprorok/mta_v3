e96073a5cb322b077db09f6221e34f2215c878b4
Walter Prorok <waltprorok@gmail.com>
2025-09-13 17:06:44 -0400

    MTA-903 fixed message listener

--- app/Listeners/SendNewTeacherMessageListener.php
+++ app/Listeners/SendNewTeacherMessageListener.php
@@ -35,7 +35,6 @@ class SendNewTeacherMessageListener
             Message::query()->create([
                 'user_id_from' => User::ADMIN_ID,
                 'user_id_to' => $user->id,
-                'subject' => 'Welcome to Music Teachers Aid',
                 'body' => 'There are a couple of things to do before running your studio.<div>
 <br><div>In the upper right hand corner click the Profile Icon and Select Settings.<br><div>
 <br><div><ol><li>Fill out the&nbsp;<a href="/teacher/studio">Studio Information Page</a>
