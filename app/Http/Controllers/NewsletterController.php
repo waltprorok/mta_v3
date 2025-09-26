9ff28df442f53ee0f26ebe430ec36cbe724d687b
Walter Prorok <waltprorok@gmail.com>
2025-09-26 15:01:38 -0400

    MTA-905 newsletter fix in config file

--- app/Http/Controllers/NewsletterController.php
+++ app/Http/Controllers/NewsletterController.php
@@ -2,24 +2,34 @@
 
 namespace App\Http\Controllers;
 
+use Exception;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
+use Log;
 use Newsletter;
 
 class NewsletterController extends Controller
 {
     public function store(Request $request): RedirectResponse
     {
-        if (empty($request->input('email'))) {
-            return back();
-        } else {
-            if (! Newsletter::isSubscribed($request->get('email'))) {
-                Newsletter::subscribePending($request->get('email'));
+        $request->validate([
+            'email' => 'required|email',
+        ]);
 
-                return back()->with('success', 'Thank You For Subscribing to the News Letter');
+        $email = $request->input('email');
+
+        try {
+            if (! Newsletter::isSubscribed($email)) {
+                Newsletter::subscribePending($email);
+
+                return back()->with('success', 'Thank you for subscribing to the newsletter!');
             }
 
-            return back()->with('error', 'Sorry! You have already subscribed');
+            return back()->with('error', 'You are already subscribed.');
+        } catch (Exception $e) {
+            Log::error('Newsletter subscription error: ' . $e->getMessage());
+
+            return back()->with('error', 'There was a problem subscribing you to the newsletter. Please try again later.');
         }
     }
 }
