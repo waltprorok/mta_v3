c501fe75c6c98ee8bb80bf6e14a086acdd8ad0f9
Walter Prorok <waltprorok@gmail.com>
2025-10-17 17:31:08 -0400

    MTA-914 pass in specific phone number to formatter

--- resources/assets/js/components/PhoneNumberFormat.vue
+++ resources/assets/js/components/PhoneNumberFormat.vue
@@ -9,13 +9,14 @@
 export default {
     name: 'PhoneNumberFormat',
     props: {
-        data: Object,
+        data: String,
+        required: true,
     },
     methods: {
         formatPhoneNumber: function () {
-            if (!this.data.phone) return '';
+            if (! this.data) return '';
 
-            const digits = this.data.phone;
+            const digits = this.data;
 
             if (digits.length === 10) {
                 return digits.replace(/(\d{3})(\d{3})(\d{4})/, '$1-$2-$3');
