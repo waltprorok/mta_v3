f9a46214ff67afe779f0f089272ddb746b70edc8
Walter Prorok <waltprorok@gmail.com>
2025-10-28 18:53:08 -0400

    MTA-923 Message updates to admin and template

--- app/Services/MessageService.php
+++ app/Services/MessageService.php
@@ -9,7 +9,7 @@ use Illuminate\Support\Facades\Auth;
 
 class MessageService
 {
-    public function getStudentTeacher()
+    public function getStudentTeacher(): Collection
     {
         $teacher = Student::where('student_id', Auth::id())->firstOrFail(['id', 'student_id', 'teacher_id']);
 
@@ -20,7 +20,7 @@ class MessageService
             ->get(['id', 'first_name', 'last_name', 'created_at', 'teacher', 'student', 'parent']);
     }
 
-    public function getStudentUsers(int $status)
+    public function getStudentUsers(int $status): Collection
     {
         if ($status == Student::PARENT) {
             return User::whereHas('parentOfStudent', function ($query) {
@@ -37,14 +37,26 @@ class MessageService
             ->get(['id', 'first_name', 'last_name', 'created_at', 'teacher', 'student', 'parent']);
     }
 
-    public function getUsers(int $status)
+    public function getUsers(int $status): ?Collection
     {
         return match (true) {
             Auth::user()->teacher => $this->getStudentUsers($status),
             Auth::user()->student => $this->getStudentTeacher(),
-            Auth::user()->parent => $this->getParentTeacher(),
+            Auth::user()->parent  => $this->getParentTeacher(),
+            Auth::user()->admin   => $this->getActiveTeachers(),
             default => null,
         };
+    }
+
+    private function getActiveTeachers(): Collection
+    {
+        return User::with(['messages' => function ($query) {
+            $query->where(['user_id_from' => Auth::id(), 'read' => false])
+                ->orderBy('id', 'desc');
+        }])
+            ->where(['is_active' => true, 'teacher' => true])
+            ->orderBy('first_name')
+            ->get(['id', 'first_name', 'last_name', 'created_at', 'teacher']);
     }
 
     private function getParentTeacher(): Collection
