eeb8ac7cc7761fe38be109eaa266220db22e3bdd
Walter Prorok <waltprorok@gmail.com>
2026-04-12 18:09:49 -0400

    MTA-946 npm update

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,18 +1,13 @@
+process.env.DISABLE_WEBPACKBAR = true;
+
 let mix = require('laravel-mix');
+const path = require('path');
 
 /*
  |--------------------------------------------------------------------------
  | Mix Asset Management
  |--------------------------------------------------------------------------
- |
- | Mix provides a clean, fluent API for defining some Webpack build steps
- | for your Laravel application. By default, we are compiling the Sass
- | file for the application as well as bundling up all the JS files.
- |
  */
-
-// mix.js('resources/assets/js/app.js', 'public/js')
-//    .sass('resources/assets/sass/app.scss', 'public/css');
 
 mix.js('resources/assets/js/app.js', 'public/js')
     .vue()
@@ -22,12 +17,12 @@ mix.js('resources/assets/js/app.js', 'public/js')
         terser: {
             extractComments: false
         }
-    })
-
-const path = require('path');
+    });
 
 mix.webpackConfig({
     stats: 'minimal',
+    plugins: [],
+
     resolve: {
         alias: {
             '@': path.resolve(__dirname, 'resources/assets/js'),
@@ -41,3 +36,9 @@ if (mix.inProduction()) {
 } else {
     mix.sourceMaps();
 }
+
+mix.override(config => {
+    config.plugins = config.plugins.filter(
+        plugin => plugin.constructor.name !== 'ProgressPlugin'
+    );
+});
