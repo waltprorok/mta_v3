07bbf5d020611b00dd356712f2e730f06e88f556
Walter Prorok <waltprorok@gmail.com>
2026-09-14 14:29:47 -0400

    MTA-948 return json in response

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -14,10 +14,11 @@ class ContactController extends Controller
 {
     public function index()
     {
-        return Contact::query()
-            ->select(['id', 'name', 'email', 'subject', 'message', 'reply', 'created_at'])
-            ->orderBy('created_at', 'desc')
+        $contacts = Contact::query()->select(['id', 'name', 'email', 'subject', 'message', 'reply', 'created_at'])
+            ->latest()
             ->get();
+
+        return response()->json($contacts);
     }
 
     public function show(Contact $contact): JsonResponse
